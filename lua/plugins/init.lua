89e0e85ad3046546bdcf339fe30d084e608aaaf8
john7002 <john_loriette@yahoo.fr>
2022-03-19 11:48:06 +0100

    Create init.lua

---
+++ lua/plugins/init.lua
@@ -0,0 +1,42 @@
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'tanvirtin/monokai.nvim'
+
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icon
+    },
+    config = function() require'nvim-tree'.setup {} end
+  }
+  use {"akinsho/toggleterm.nvim"}
+  use 'neovim/nvim-lspconfig' -- Collection of configurations for the built-in LSP client
+  use 'williamboman/nvim-lsp-installer'
+  use 'hrsh7th/cmp-nvim-lsp' -- for completion
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-cmdline'
+  use 'saadparwaiz1/cmp_luasnip'
+
+  --snippets
+  use 'L3MON4D3/LuaSnip'
+  use 'rafamadriz/friendly-snippets'
+
+
+  --comment
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+        require('Comment').setup()
+    end
+  }
+
+end)
