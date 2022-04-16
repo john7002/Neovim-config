9c2eb8a9b4e5132bfd23ebd8120176d172603844
john7002 <john_loriette@yahoo.fr>
2022-04-16 19:44:35 +0200

    Update init.lua
    add alpha-vim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -43,5 +43,12 @@ return require('packer').startup(function()
         require('Comment').setup()
     end
   }
+  use({
+		"goolord/alpha-nvim",
+		requires = { "kyazdani42/nvim-web-devicons" },
+		config = function()
+			require("alpha").setup(require("alpha.themes.startify").config)
+		end,
+	})
 
 end)
